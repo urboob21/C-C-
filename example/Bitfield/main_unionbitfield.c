49f5864cf7004622a84509d711e269cb81b6c7f4
Phong Nguyen <118428350+urboob21@users.noreply.github.com>
2024-01-07 15:34:12 +0700

    [#001] Union bit field

---
+++ example/Bitfield/main_unionbitfield.c
@@ -0,0 +1,40 @@
+/*
+MSB            LSB
+ 7              0
++--------------------------+
+| 4 bits(cmd) | 4bits(data)|
++--------------------------+
+
+send 1byte data: 4 bits cmd + 4 bits data
+
+-> parse
+
+
+*/
+#include "stdio.h"
+#include "stdint.h"
+
+typedef struct
+{
+    //  LSB
+    uint8_t data : 4 ;
+    uint8_t cmd : 4;
+    // MS B
+} fields_tag_t;
+
+typedef union
+{
+    uint8_t rawInput;
+    fields_tag_t fields;
+} input_data8_t;
+
+int main()
+{
+    input_data8_t input;
+    input.rawInput = 0b11000100; //  raw: 0xc4  
+    printf("Value of raw input is: 0x%x \n", input.rawInput); 
+    printf("Value of cmd is:  0x%x\n", input.fields.cmd);        // 0xc
+    printf("Value of data is:  0x%x \n", input.fields.data);    // 0x4
+
+    return 0;
+}
